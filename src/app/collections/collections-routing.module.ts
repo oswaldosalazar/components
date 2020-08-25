62e9d37d15f9f69adfb6d92cf403f181da3c245a
oswaldosalazar <oswaldo.r.salazar@gmail.com>
2020-08-24 19:18:22 -0600

    Added lazy loading for elements and collections, added placeholder to elements

--- src/app/collections/collections-routing.module.ts
+++ src/app/collections/collections-routing.module.ts
@@ -3,12 +3,10 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { CollectionsHomeComponent } from './collections-home/collections-home.component';
 
-const routes: Routes = [
-  { path: 'collections', component: CollectionsHomeComponent },
-];
+const routes: Routes = [{ path: '', component: CollectionsHomeComponent }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule],
+  exports: [RouterModule]
 })
 export class CollectionsRoutingModule {}
