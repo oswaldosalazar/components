71dd33c22cc200dc737d85ad80684ce61b5daa2e
oswaldosalazar <oswaldo.r.salazar@gmail.com>
2020-09-05 18:37:55 -0600

    Finished resusable table comp

--- src/app/collections/table/table.component.ts
+++ src/app/collections/table/table.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-table',
@@ -6,10 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./table.component.css']
 })
 export class TableComponent implements OnInit {
+  @Input('class') classNames = '';
 
-  constructor() { }
+  @Input() data = [];
+  @Input() headers = [];
 
-  ngOnInit(): void {
-  }
+  constructor() {}
 
+  ngOnInit(): void {}
 }
